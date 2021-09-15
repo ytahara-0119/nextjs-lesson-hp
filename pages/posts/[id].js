1f683f71c194b86b4af4a3d3e24098b6276d35bc
田原芳信 <taharayoshinobu@MacBook-Air.local>
2021-09-16 02:35:23 +0900

    modified error property

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -24,9 +24,9 @@ export default function Post({ post }) {
             viewBox="0 0 24 24" 
             xmlns="http://www.w3.org/2000/svg">
               <path 
-                stroke-linecap="round" 
-                stroke-linejoin="round" 
-                stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7">
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                strokeWidth="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7">
               </path>
           </svg>
           <span>Back to blog-page</span>
